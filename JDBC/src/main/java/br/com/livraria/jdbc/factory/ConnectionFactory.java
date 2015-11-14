e6742330f31ee4c1eb4ec31aaea1335ec624c1bc
Felipe Lima <flf.felipe@gmail.com>
2015-11-13 23:47:29 -0200

    Verifica se conexão com o banco já foi criada.

--- JDBC/src/main/java/br/com/livraria/jdbc/factory/ConnectionFactory.java
+++ JDBC/src/main/java/br/com/livraria/jdbc/factory/ConnectionFactory.java
@@ -6,24 +6,27 @@ import com.mysql.jdbc.Connection;
 
 public class ConnectionFactory 
 {
+	private static Connection conexao = null;
+	
 	public static Connection createConnection()
 	{
-
-		String stringDeConexao = "jdbc:mysql://localhost:3306/livraria";
-		String usuario = "root";
-		String senha = "admin";
-
-		Connection conexao = null;
-		
-		try 
+		if (conexao == null)
 		{
-			conexao = (Connection) DriverManager.getConnection(stringDeConexao, usuario, senha);
-		} 
-		catch (Exception e) 
-		{
-			e.printStackTrace();
+			String stringDeConexao = "jdbc:mysql://localhost:3306/livraria";
+			String usuario = "root";
+			String senha = "admin";
+	
+			conexao = null;
+			
+			try 
+			{
+				conexao = (Connection) DriverManager.getConnection(stringDeConexao, usuario, senha);
+			} 
+			catch (Exception e) 
+			{
+				e.printStackTrace();
+			}
 		}
-		
 		return conexao;
 	}
 }
